10c7dc0b72fab5d5d57b91d135272ead3c418242
erlynorbel <lebron_paulmark_dimayacyac@dlsl.edu.ph>
2024-09-12 15:04:25 -0400

    feat: added classes

--- class-diagram-and-objects.cpp
+++ class-diagram-and-objects.cpp
@@ -1,7 +1,58 @@
 #include <iostream>
 
 using namespace std;
+class Customer{
+private:
+    string customerId;
+    string customerName;
+    string email; 
+    string address;
 
+public:
+    void addtoCart(){
+
+    }
+
+};
+
+class Product{
+private:
+    string productId; 
+    string name;
+    string description;
+    float price;
+
+public:
+    void updateStock(int quantity){
+
+    }
+
+    string getDetail(){
+        return "Product Detail";
+    }
+};
+class Order{
+    private:
+    string orderId;
+    string orderDate;
+    string orderStatus;
+    public:
+
+    Product product; 
+
+    void placeOrder(){
+
+    }
+    void cancelOrder(){
+
+    }
+};
+
+class ShoppingCart{
+    private:
+    string cartId;
+
+};
 int main(){
-    cout << "Lemaw"; 
+
 }
